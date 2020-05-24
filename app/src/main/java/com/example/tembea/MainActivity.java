37bdef7ab86c81dd999e2aba0a54a07cc8cf8f6e
paulo-789 <jorhnepaulone@gmail.com>
2020-05-24 20:10:34 +0300

    added location intent

--- app/src/main/java/com/example/tembea/MainActivity.java
+++ app/src/main/java/com/example/tembea/MainActivity.java
@@ -6,20 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button mfindSitesButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationEditText=(EditText)findViewById(R.id.locationEditText) ;
         mfindSitesButton = (Button)findViewById(R.id.findSitesButton);
         mfindSitesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this,Places.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
 
             }
