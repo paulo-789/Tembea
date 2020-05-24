6c8425670081874fc8e3a935a2bf076e007fe824
paulo-789 <jorhnepaulone@gmail.com>
2020-05-24 21:46:31 +0300

    added location intent in place

--- app/src/main/java/com/example/tembea/Places.java
+++ app/src/main/java/com/example/tembea/Places.java
@@ -2,16 +2,20 @@ package com.example.tembea;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Places extends AppCompatActivity {
+    private TextView mlocationTextView;
+
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.places);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+        mlocationTextView.setText(location);
     }
 }
