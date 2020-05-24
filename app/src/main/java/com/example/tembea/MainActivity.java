aba600093aa252d274e470337b91e4cd11568c6d
paulo-789 <jorhnepaulone@gmail.com>
2020-05-24 19:41:08 +0300

    added intent

--- app/src/main/java/com/example/tembea/MainActivity.java
+++ app/src/main/java/com/example/tembea/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.tembea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mfindSitesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mfindSitesButton = (Button)findViewById(R.id.findSitesButton);
+        mfindSitesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Places.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/tembea/Places.java
@@ -0,0 +1,4 @@
+package com.example.tembea;
+
+public class Places {
+}
