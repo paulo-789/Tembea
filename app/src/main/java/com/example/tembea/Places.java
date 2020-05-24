4bef042babcbd8974f0be2c1315fa5ad25d3171a
paulo-789 <jorhnepaulone@gmail.com>
2020-05-24 20:18:02 +0300

    override my main class

--- app/src/main/java/com/example/tembea/Places.java
+++ app/src/main/java/com/example/tembea/Places.java
@@ -1,4 +1,13 @@
 package com.example.tembea;
 
-public class Places {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Places extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
