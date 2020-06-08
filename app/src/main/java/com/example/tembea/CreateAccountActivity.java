21c86435aef8f199372c79dd64696c1de67adf9e
paulo-789 <jorhnepaulone@gmail.com>
2020-06-08 15:53:27 +0300

    added menu file for the overflow menu

--- app/src/main/java/com/example/tembea/CreateAccountActivity.java
+++ app/src/main/java/com/example/tembea/CreateAccountActivity.java
@@ -31,7 +31,6 @@ public class CreateAccountActivity extends AppCompatActivity implements  View.On
     @BindView(R.id.loginTextView) TextView mLoginTextView;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,23 +40,23 @@ public class CreateAccountActivity extends AppCompatActivity implements  View.On
         mLoginTextView.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
         mAuth = FirebaseAuth.getInstance();
-//        createAuthStateListener();
+        createAuthStateListener();
     }
 
-//    private void createAuthStateListener() {
-//        mAuthListener = new FirebaseAuth.AuthStateListener() {
-//            @Override
-//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-//                final FirebaseUser user = firebaseAuth.getCurrentUser();
-//                if (user != null) {
-//                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
-//                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//                    startActivity(intent);
-//                    finish();
-//                }
-//            }
-//        };
-//    }
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
 
     @Override
     public void onClick(View view) {
@@ -94,17 +93,17 @@ public class CreateAccountActivity extends AppCompatActivity implements  View.On
 
     }
 
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        mAuth.addAuthStateListener(mAuthListener);
-//    }
-//
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        if (mAuthListener != null) {
-//            mAuth.removeAuthStateListener(mAuthListener);
-//        }
-//    }
+   @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 }
