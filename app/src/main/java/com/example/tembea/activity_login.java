3b23508bb88e83fd07aa45867c26d3b022208778
paulo-789 <jorhnepaulone@gmail.com>
2020-06-08 08:40:16 +0300

    registered my account in fire base

--- app/src/main/java/com/example/tembea/activity_login.java
+++ app/src/main/java/com/example/tembea/activity_login.java
@@ -2,13 +2,33 @@ package com.example.tembea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class activity_login extends AppCompatActivity {
+import com.mapbox.services.android.navigation.ui.v5.NavigationContract;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class activity_login extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(activity_login.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
